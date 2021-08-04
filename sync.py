639e7fa5b2c0faa9d85d022e39c7910c58457def
Tushar Thakur <thakur.tushar0205@gmail.com>
2021-08-04 16:29:27 +1200

    change to bezier

--- sync.py
+++ sync.py
@@ -1,14 +1,27 @@
 import verse1
 import verse2
 import chorus
+import httplib, urllib
+import requests
+from threading import Thread
+from naoqi import ALProxy
+
+# IP address of leader robot
+flask_ip = "192.168.0.101:5000"
+flaskWebIP = "http://192.168.0.101:5000"
 
 
 def sync(motion, is_verse1):
+    behavior_mng_service = ALProxy("ALBehaviorManager", "192.168.0.100", 9559)
+    behavior_mng_service.runBehavior("stand-166579/behavior_1", _async=True)
+
     if is_verse1:
         [names, times, keys] = verse1.verse1()
     else:
         [names, times, keys] = verse2.verse2()
-    motion.angleInterpolation(names, keys, times, True)
 
-    [names, times, keys] = chorus.chorus()
-    motion.angleInterpolation(names, keys, times, True)
+    [chorus_names, chorus_times, chorus_keys] = chorus.chorus()
+
+    motion.angleInterpolationBezier(names, times, keys)
+
+    motion.angleInterpolationBezier(chorus_names, chorus_times, chorus_keys)
